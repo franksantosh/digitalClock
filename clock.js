1a13c1468b87b94c8be9268bc71da43deecbe03f
franksantosh <franksantosh12@gmail.com>
2018-05-01 12:32:38 +0545

    digital clock for different country

---
+++ clock.js
@@ -0,0 +1,66 @@
+var state = ['Sydney','Berlin','Newyork','Kathmandu'];
+var layout = "<div class='clockbox'>"+
+			"</div>"+
+    	"<div class='digital'>"+
+    	"<h3 id='place'></h3>"+
+        "<h4 id='time'></h4>"+
+    		"</div>";
+
+function main(){
+	for (var i = 0; i < state.length; i++) {
+		var element = document.getElementsByClassName(state[i])[0].innerHTML = layout;
+	}
+}
+
+document.addEventListener('DOMContentLoaded', main);
+
+var HOUR, MINUTE, SECOND, TIME, PLACE;
+
+function count(){
+	HOUR = document.querySelectorAll("#hour");
+	MINUTE = document.querySelectorAll("#minute");
+	SECOND = document.querySelectorAll("#second");
+	TIME = document.querySelectorAll('#time');
+	PLACE = document.querySelectorAll('#place'); 
+}
+var counter = setInterval(count, 10);
+setTimeout(function(){
+	clearInterval(counter);
+	for (var i = 0; i < state.length; i++) {
+		PLACE[i].innerHTML = state[i];
+	}
+	setInterval(function(){
+		getDate(+10, 0);
+	}, 1000);
+	setInterval(function(){
+			getDate(+2, 1);
+		}, 1000);
+	setInterval(function(){
+			getDate(-4, 2);
+		}, 1000);
+	setInterval(function(){
+			getDate(+5.75, 3);
+		}, 1000);
+},  1000);
+
+function getDate(x, y){
+	
+	var time = new Date();
+	var utc = time.getTime() + time.getTimezoneOffset() * 60000;
+	var stateTime = new Date(utc + (3600000*(x)))
+	var minutes = stateTime.getMinutes();
+	var seconds = stateTime.getSeconds();
+	var hr = stateTime.getHours();
+	var session="AM";
+	if (hr==0) {
+				hr==12;
+			}
+			if (hr>12) {
+				hr=hr-12;
+				session="PM"
+			}
+			hr=(hr<10) ? "0" + hr : hr;
+			minutes=(minutes<10) ? "0" + minutes : minutes;
+			seconds=(seconds<10) ? "0" + seconds : seconds;
+	TIME[y].innerHTML = 'Time: ' + hr + ':' + minutes + ':'+seconds + ''+''+session;
+}
